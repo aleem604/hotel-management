ac6733c6daee2d345394f42c97535fd8c45f4e5a
aleem <aleem604@gmail.com>
2023-12-17 16:53:33 +0500

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <div>hellow World</div>;
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <h1>hellow World</h1>
+        <button>Check in</button>
+      </div>
+    </>
+  );
 }
+
+export default App;
